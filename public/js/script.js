421c48eed0b623d2409592b78995967d77c4e130
Emmanuella Amonoo <amonooemma2@gmail.com>
2024-06-06 15:58:04 +0000

    Math in Javascript

--- public/js/script.js
+++ public/js/script.js
@@ -72,7 +72,42 @@ const tweet1 = {
       console.log('notify friends!', i)
     }
     
-    
+    //Functions
+//Defining functions
+function login(username, password) {
+  if (username == 'mickeymond' && password == '1234') {
+    return 'user is logged in';
+  } else {
+    return 'invalid username or password' ;
+  }
+}
+//invoking functions
+  login('mickeymond', '1234');
+  
+
+//Basic Arithmatic
+
+12 + 34;
+45 - 16;
+7 * 12;
+78 / 4;
+78 % 4;
+
+// Comparison operators 
+
+2 == 2;
+3 !== 2;
+
+//Built in Math Functions
+Math.round(37/24);
+Math.floor(37/24);
+Math.random();
+Math.ceil(37/24);
+Math.max(12, 14, 89)
+//order of operations
+1 * 2 + 3 -4 / 2 % 2**2;
+//handling user inputs
+1200 + Number('250');
     
     
     
